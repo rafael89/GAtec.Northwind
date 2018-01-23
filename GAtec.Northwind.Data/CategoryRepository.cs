e8d82dc043305dcf32d93d2ca767bc8abdf52db2
Rafael Oliveira <rafael.oliveira@gatec.com.br>
2018-01-23 19:11:25 -0200

    c1

---
+++ GAtec.Northwind.Data/CategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using GAtec.Northwind.Domain.Model;
+using GAtec.Northwind.Domain.Repository;
+
+namespace GAtec.Northwind.Data
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public void Add(Category item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Update(Category item)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Category Get(object id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Category> GetAll()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
